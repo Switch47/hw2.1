d2ae68c6e7fa80e36b1314aba8d4d3ea165b2e33
Switch47 <kongkiat.swb@gmail.com>
2021-05-20 16:16:54 +0700

    first commit

---
+++ src/main/java/org/example/hw2/Main.java
@@ -0,0 +1,9 @@
+package org.example.hw2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var simulator = new Simulator();
+        simulator.simulate(1000);
+    }
+}
